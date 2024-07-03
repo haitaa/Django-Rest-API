7a838bcc92d58d1a42486fb5630c4401549935f1
Mustafa Haita <mustafahaitaa@gmail.com>
2024-07-03 17:18:26 +0300

    Related Fields & Foreign Key Serializer

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -3,16 +3,29 @@ from rest_framework.reverse import reverse
 
 from .models import Product
 from .validators import validate_title_no_hello, unique_product_title
+from api.serializers import UserPublicSerializer
+
+class ProductInlineSerializer(serializers.Serializer):
+    url = serializers.HyperlinkedIdentityField(view_name='product-detail', lookup_field='pk', read_only=True)
+    title = serializers.CharField(read_only=True)
 
 class ProductSerializer(serializers.ModelSerializer):
+    owner = UserPublicSerializer(source='user', read_only=True)
+    related_products = ProductInlineSerializer(source="user.product_set.all", read_only=True, many=True)
+    my_user_data = serializers.SerializerMethodField(read_only=True)
     my_discount = serializers.SerializerMethodField(read_only=True)
     edit_url = serializers.SerializerMethodField(read_only=True)
     url = serializers.HyperlinkedIdentityField(view_name="product-detail", lookup_field="pk")
     title = serializers.CharField(validators=[validate_title_no_hello, unique_product_title])
-    #email = serializers.EmailField(write_only=True)
+    #email = serializers.CharField(source='user.email', read_only=True)
     class Meta:
         model = Product
-        fields = ['url', 'edit_url', 'pk', "title", "content", "price", "sale_price", "my_discount"]
+        fields = ['owner', 'url', 'edit_url', 'pk', "title", "content", "price", "sale_price", "my_discount", 'my_user_data', 'related_products']
+
+    def get_my_user_data(self, obj):
+        return {
+            "username": obj.user.username
+        }
 
     # def validate_title(self, value):
     #     request = self.context.get('request')
