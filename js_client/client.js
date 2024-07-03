c71290dc358f63789e703d0dda013e961fae3f80
Mustafa Haita <mustafahaitaa@gmail.com>
2024-07-03 22:23:46 +0300

    Using JWT with JS Client

--- js_client/client.js
+++ js_client/client.js
@@ -1,3 +1,4 @@
+const contentContainer = document.getElementById("content-container");
 const loginForm = document.getElementById("login-form");
 const baseEndpoint = "http://localhost:8000/api";
 if (loginForm) {
@@ -21,10 +22,60 @@ function handleLogin(event) {
         .then((response) => {
             return response.json();
         })
-        .then((x) => {
-            console.log(x);
+        .then((authData) => {
+            handleAuthData(authData, getProductList);
         })
         .catch((err) => {
             console.log("err", err);
+        });
+}
+
+function handleAuthData(authData, callback) {
+    localStorage.setItem("access", authData.access);
+    localStorage.setItem("refresh", authData.refresh);
+    if (callback) {
+        callback();
+    }
+}
+
+function writeToContainer(data) {
+    if (contentContainer) {
+        contentContainer.innerHTML =
+            "<pre>" + JSON.stringify(data, null, 4) + "</pre>";
+    }
+}
+
+function getFetchOptions(method, body) {
+    return {
+        method: method === null ? "GET" : method,
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem("access")}`,
+        },
+        body: body ? body : null,
+    };
+}
+
+function isTokenNotValid(jsonData) {
+    if (jsonData.code && jsonData.code === "token_not_valid") {
+        alert("Please log again!");
+        return false;
+    }
+    return true;
+}
+
+function getProductList() {
+    const endpoint = `${baseEndpoint}/products/`;
+    const options = getFetchOptions();
+    fetch(endpoint, options)
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            console.log(data);
+            const validData = isTokenNotValid(data);
+            if (validData) {
+                writeToContainer(data);
+            }
         });
 }
