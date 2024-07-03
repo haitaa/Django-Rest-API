9f2da98cb7ac9b806149adefb40496bcf37a7ca2
Mustafa Haita <mustafahaitaa@gmail.com>
2024-07-03 21:40:05 +0300

    Login via JavaScript Client

---
+++ js_client/client.js
@@ -0,0 +1,30 @@
+const loginForm = document.getElementById("login-form");
+const baseEndpoint = "http://localhost:8000/api";
+if (loginForm) {
+    // handle this login form
+    loginForm.addEventListener("submit", handleLogin);
+}
+
+function handleLogin(event) {
+    event.preventDefault();
+    const loginEndpoint = `${baseEndpoint}/token/`;
+    let loginFormData = new FormData(loginForm);
+    let loginObjectData = Object.fromEntries(loginFormData);
+    const options = {
+        method: "POST",
+        headers: {
+            ContentType: "application/json",
+        },
+        body: JSON.stringify(loginObjectData),
+    };
+    fetch(loginEndpoint, options)
+        .then((response) => {
+            return response.json();
+        })
+        .then((x) => {
+            console.log(x);
+        })
+        .catch((err) => {
+            console.log("err", err);
+        });
+}
